5dfe90df8b83379844d30dc4fa89c67a83975b97
ecmapps <72906286+ecmapps@users.noreply.github.com>
2023-07-18 02:24:55 +0000

    Primer push funcional

--- src/app.js
+++ src/app.js
@@ -7,5 +7,40 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const numbers = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+  const symbols = ["♥", "♦", "♠", "♣"];
+
+  cardGenerator(numbers, symbols);
 };
+function cardGenerator(arr1, arr2) {
+  let number = randomValue(arr1);
+  let symbol = randomValue(arr2);
+  let color = symbol == "♥" || symbol == "♦" ? "red" : "black";
+  //Changing card info
+  let colorDom = document.querySelector(".card");
+  colorDom.classList.add(color == "red" ? "red" : "black");
+  let symbolDom = document.querySelectorAll(".symbol");
+  for (let i = 0; i < symbolDom.length; i++) {
+    symbolDom[i].innerHTML = symbol;
+  }
+  document.querySelector(".number").innerHTML = number;
+}
+
+function randomValue(arr) {
+  let value = Math.floor(Math.random() * arr.length);
+  return arr[value];
+}
